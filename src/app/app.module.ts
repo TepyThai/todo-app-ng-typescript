db7ee2f06898fa3e2edf82a3b2a2e8ee2e6520c8
Tepy Thai <thaitepy@gmail.com>
2017-05-09 10:03:52 +0900

    css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
-  ],
+    AppComponent,
+  TodoListHeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
